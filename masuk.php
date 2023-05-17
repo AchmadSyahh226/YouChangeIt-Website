2ba53c5ffcac90158316399c5a851d1cbcc44a26
AchmadSyahh226 <94022829+AchmadSyahh226@users.noreply.github.com>
2023-05-17 07:12:16 +0700

    Mengganti lokasi halaman selanjutnya

--- masuk.php
+++ masuk.php
@@ -8,7 +8,7 @@ $sql = "SELECT * FROM tb_users WHERE email = '$email' AND password = '$password'
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-    header("Location: index.html");
+    header("Location: utama.html");
 } else {
     echo "Masuk akun gagal: " . mysqli_error($conn);
 }
